6708cf1300710d6dc16b252698f063ad2cd3fdfe
fn00eu <fn00eu@proton.me>
2024-03-01 20:02:54 +0100

    clean the filename

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -18,6 +18,7 @@ func StrToBigInt(s string) (amount *big.Int, success bool) {
 }
 
 func LoadConfiguration(filename string) (*shared.Configuration, error) {
+	filename = filepath.Clean(filename)
 	filename = filepath.Join("./configs", filepath.Base(filename))
 
 	data, err := os.ReadFile(filename)
